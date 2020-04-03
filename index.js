28f7648796f49e4ce481f26e85220510389f8364
Nick VanderMolen <tree.vandermolen@gmail.com>
2020-04-03 23:25:59 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function takeANumber(line, name){
+    
+} return 'Welcome, ${name}. You are number ${line} in line.'
+
+
+function nowServing(katzDeliLine){
+  var i = 0
+  if(katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!"}
+  else {
+    for(i = 0; i<katzDeliLine.length; i++){
+  var guest = katzDeliLine.shift()
+  return 'Currently serving ${guest}.'}
+  }
+}
